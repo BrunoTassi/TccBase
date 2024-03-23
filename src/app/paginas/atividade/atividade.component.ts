c01146f622c2e58caba49739ce0345747d68984e
Bruno Tassi <brunoxtassi923@gmail.com>
2024-03-23 14:01:18 -0300

    up final

--- src/app/paginas/atividade/atividade.component.ts
+++ src/app/paginas/atividade/atividade.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { Modal } from 'bootstrap';
+
+const openModalButton = document.getElementById('openModalButton');
+
+
 
 @Component({
   selector: 'app-atividade',
@@ -6,5 +11,41 @@ import { Component } from '@angular/core';
   styleUrl: './atividade.component.css'
 })
 export class AtividadeComponent {
+
+  constructor(private el: ElementRef) {
+    if (openModalButton) { // Verificar se o elemento foi encontrado
+      openModalButton.addEventListener('click', function () {
+        // Selecionar o elemento do modal pelo ID
+        const modalElement = document.getElementById('exampleModal');
+        if (modalElement) { // Verificar se o elemento do modal foi encontrado
+          // Criar uma instância de Modal passando o elemento do modal
+          const modalInstance = new Modal(modalElement);
+          // Mostrar o modal
+          modalInstance.show();
+        }
+      });
+    }
+  }
+ 
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    const key = event.key;
+    if (isNaN(Number(key)) && key !== 'Backspace' && key !== 'Delete') {
+      event.preventDefault();
+    }
+  }
+
+  @HostListener('input', ['$event'])
+  onInput(event: InputEvent) {
+    const input = event.target as HTMLInputElement;
+    const valor = input.value.replace(/\D/g, ''); // Remove todos os caracteres que não são dígitos
+
+    if (valor.length > 11) {
+      input.value = valor.slice(0, 11); // Limita a 11 dígitos
+    }
+  }
+
+
 
 }
