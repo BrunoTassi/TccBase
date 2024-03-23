59c32e837553df72b377a6223c9ebcf4fa1931a6
Bruno Tassi <brunoxtassi923@gmail.com>
2024-03-22 21:34:46 -0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
-import { ArtigosComponent } from './paginas/artigos/artigos.component';
 import { SobreComponent } from './paginas/sobre/sobre.component';
 import { AtividadeComponent } from './paginas/atividade/atividade.component';
+import { ArtigosComponent } from './paginas/artigos/artigos.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },
   {
     path: 'home',
     component: HomeComponent
--- src/app/paginas/artigos/artigos.component.ts
+++ src/app/paginas/artigos/artigos.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-artigos',
   templateUrl: './artigos.component.html',
-  styleUrl: './artigos.component.css'
+  styleUrls: ['./artigos.component.css']
 })
 export class ArtigosComponent {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    //document.addEventListener('DOMContentLoaded', () => {
+    //  // Selecione e role até o destino
+    //  this.scrollToDestination();
+    //});
+  }
+
+  //scrollToDestination() {
+  //  const destino = document.querySelector('#secao01');
+  //  if (destino) {
+  //    destino.scrollIntoView({
+  //      behavior: 'smooth'
+  //    });
+  //  }
+  //}
 }
